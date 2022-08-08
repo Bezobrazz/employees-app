37cd021634fb977f1df7aaad8e319252433a6533
Viacheslav Kostenko <103434668+Bezobrazz@users.noreply.github.com>
2022-08-08 13:40:47 +0300

    rise and increase salary functionality added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,6 +6,11 @@ import EmployeesAddForm from "../employees-add-form/employees-add-form";
 import "./app.css";
 
 function App() {
+  const data = [
+    { name: "John C.", salary: 700, increase: true, id: 1 },
+    { name: "Maria D.", salary: 2300, increase: false, id: 2 },
+    { name: "Serg D.", salary: 4100, increase: false, id: 3 },
+  ];
   return (
     <div className="app">
       <AppInfo />
@@ -13,7 +18,7 @@ function App() {
         <SearchPanel />
         <AppFilter />
       </div>
-      <EmployeesList />
+      <EmployeesList data={data} />
       <EmployeesAddForm />
     </div>
   );
